75203f7d6d2849a9640845bd565b7769780b605e
Pranay Jha <63332027+pranaysjha@users.noreply.github.com>
2023-02-12 21:23:25 -0800

    typos

--- rolling_sharpe.py
+++ rolling_sharpe.py
@@ -14,19 +14,14 @@ def rolling_sharpe(daily_returns: pd.Series,
     Returns:
         pd.Series:  
     """
-
     DAYS_IN_MONTH = 21
-    
-    # Compute D-hat
+    # Compute D-bar
     avg_return = daily_returns.rolling(window = window * DAYS_IN_MONTH).mean()
     periodized_avg_return = avg_return * window * DAYS_IN_MONTH
     avg_diff_return = periodized_avg_return - risk_free_rate
-
     # Compute sigma
     volatility = daily_returns.rolling(window = window * DAYS_IN_MONTH).std()
     periodized_volatility = volatility * np.sqrt(window)
-    
-    # Compute rolling Sharpe ratio: D-hat / sigma
+    # Compute rolling Sharpe ratio: D-bar / sigma
     rolling_sharpe_ratio = (avg_diff_return / periodized_volatility).dropna()
-    
     return rolling_sharpe_ratio
